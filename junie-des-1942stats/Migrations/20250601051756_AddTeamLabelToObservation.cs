fb765761b197263ef024fcaeca4e9c2fc938a73d
Dylan Munyard <dylanmunyard@sixpivot.com.au>
2025-06-01 16:17:38 +1000

    Add migrations

---
+++ junie-des-1942stats/Migrations/20250601051756_AddTeamLabelToObservation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace junie_des_1942stats.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeamLabelToObservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TeamLabel",
+                table: "PlayerObservations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeamLabel",
+                table: "PlayerObservations");
+        }
+    }
+}
