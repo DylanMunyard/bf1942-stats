369c0a8be7e61010b8c0736be71221ef7a76ca27
DylanMunyard <dmunyard@gmail.com>
2025-06-10 22:51:44 +1000

    Remove unuseful insights

--- junie-des-1942stats/ServerStats/Models/ServerInsights.cs
+++ junie-des-1942stats/ServerStats/Models/ServerInsights.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-
 namespace junie_des_1942stats.ServerStats.Models
 {
     public class ServerInsights
@@ -10,8 +7,6 @@ namespace junie_des_1942stats.ServerStats.Models
         public DateTime StartPeriod { get; set; }
         public DateTime EndPeriod { get; set; }
         public PingByHourInsight? PingByHour { get; set; }
-        public ScoreVolatilityInsight? MostVolatilePlayers { get; set; }
-        public ScoreConsistencyInsight? LeastConsistentPlayers { get; set; }
     }
 
     public class PingByHourInsight
@@ -25,34 +20,5 @@ namespace junie_des_1942stats.ServerStats.Models
         public double AveragePing { get; set; }
         public double MedianPing { get; set; }
         public double P95Ping { get; set; }
-    }
-
-    public class ScoreVolatilityInsight
-    {
-        public List<VolatilePlayer> Players { get; set; } = [];
-    }
-
-    public class VolatilePlayer
-    {
-        public string PlayerName { get; set; } = "";
-        public int TotalNegativeDeltas { get; set; }
-        public int SessionsAnalyzed { get; set; }
-        public double AverageNegativeDeltasPerSession { get; set; }
-        public int LargestSingleDrop { get; set; }
-        public int LargestDropSessionId { get; set; }
-    }
-
-    public class ScoreConsistencyInsight
-    {
-        public List<InconsistentPlayer> Players { get; set; } = [];
-    }
-
-    public class InconsistentPlayer
-    {
-        public string PlayerName { get; set; } = "";
-        public double PercentageDecreasingObservations { get; set; }
-        public int TotalObservations { get; set; }
-        public int DecreasingObservations { get; set; }
-        public int SessionsAnalyzed { get; set; }
     }
 } 
