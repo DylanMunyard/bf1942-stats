1cdfd69be6f9cae87e9ac4d22c10f4e4e630118c
Dylan Munyard <dmunyard@gmail.com>
2025-11-02 14:50:53 +1000

    Try to fix schema out of sync

---
+++ junie-des-1942stats/Migrations/20251102044957_UpdateSnapshot2112025.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace junie_des_1942stats.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSnapshot2112025 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TournamentMatchResults_MapId",
+                table: "TournamentMatchResults");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TournamentMatchResults_MapId",
+                table: "TournamentMatchResults",
+                column: "MapId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TournamentMatchResults_MapId",
+                table: "TournamentMatchResults");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TournamentMatchResults_MapId",
+                table: "TournamentMatchResults",
+                column: "MapId");
+        }
+    }
+}
